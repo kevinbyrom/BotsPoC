492f5e00e562de06aff6cf28ee7e3b1fc43a1cbd
Kevin Byrom <kevinbyrom@kevins-mbp.lan>
2018-12-03 09:04:14 -0800

    initial commit

---
+++ BotsPoC/Processor.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+
+namespace BotsPoC
+{
+    public struct Instruction
+    {
+        public int Code;
+    }
+
+    public class Processor
+    {
+        public int AX;
+        public int BX;
+        public int T;
+        public Instruction[] Code;
+        public int CodePtr;
+
+        public Processor()
+        {
+        }
+
+        public void NextCycle()
+        {
+
+        }
+
+        public void Jmp()
+        {
+            int pos = this.AX;
+
+            if (pos >= this.Code.Length)
+                throw new Exception();
+
+            this.CodePtr = this.AX;
+        }
+
+        public void Test()
+        {
+            if (this.AX == this.BX)
+                this.T = 1;
+            else
+                this.T = 0;
+        }
+    }
+
+    /*
+     * MOV 1 AX
+     * MOV 2 BX
+     * TGE AX BX
+     * 
+    */
+}
---
+++ BotsPoC/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BotsPoC
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
